732f24c3054b7a6ec3d8677ae4f39ad4608c2b3a
Josep R <sony_s07@hotmail.es>
2021-03-25 14:58:04 -0500

    final commit

--- lib/src/UI/home.dart
+++ lib/src/UI/home.dart
@@ -16,7 +16,6 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController nombreBanda = TextEditingController();
-
   List<Band> bandas = [];
 
   @override
@@ -26,12 +25,6 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
-  _handleActiveBands(dynamic payload) {
-    this.bandas =
-        (payload as List).map((banda) => Band.fromMap(banda)).toList();
-    setState(() {});
-  }
-
   @override
   void dispose() {
     final socketService = Provider.of<SocketService>(context, listen: false);
@@ -39,80 +32,127 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
+  _handleActiveBands(dynamic payload) {
+    this.bandas =
+        (payload as List).map((banda) => Band.fromMap(banda)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     final socketService = Provider.of<SocketService>(context);
+    final size = MediaQuery.of(context).size;
+
     Map<String, double> dataMap = {};
     bandas.forEach((banda) {
       dataMap.putIfAbsent(banda.nombre, () => banda.votes.toDouble());
     });
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Band Names'),
-          centerTitle: true,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: socketService.serverStatus == ServerStatus.Online
-                  ? Icon(Icons.check_circle, color: Colors.white)
-                  : Icon(Icons.offline_bolt, color: Colors.red),
+
+    return bandas.isEmpty
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Band Names'),
+              centerTitle: true,
+              elevation: 2.0,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: socketService.serverStatus == ServerStatus.Online
+                      ? Icon(Icons.check_circle, color: Colors.white)
+                      : Icon(Icons.offline_bolt, color: Colors.red),
+                ),
+              ],
             ),
-          ],
-          elevation: 2.0,
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: PieChart(
-                dataMap: dataMap,
-                chartRadius: 200.0,
-              ),
+            body: Column(
+              children: [
+                Container(
+                  height: size.height * .3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: dataMap.isEmpty
+                        ? CircularProgressIndicator()
+                        : PieChart(
+                            dataMap: dataMap,
+                            chartRadius: 200.0,
+                          ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: bandas.length,
+                    itemBuilder: (context, index) {
+                      final Band banda = bandas[index];
+                      return Dismissible(
+                        key: Key(banda.id),
+                        direction: DismissDirection.startToEnd,
+                        background: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          color: Colors.purple,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Alaos',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 21.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        onDismissed: (direction) =>
+                            socketService.emit('delete_band', {'id': banda.id}),
+                        child: ListTile(
+                          title: Text(banda.nombre),
+                          subtitle: Text('Id: ${banda.id}'),
+                          leading: CircleAvatar(
+                            radius: 20.0,
+                            backgroundColor: Colors.blue,
+                            child: Text(
+                              banda.nombre.substring(0, 2),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          trailing: Text(
+                            banda.votes.toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 21.0,
+                            ),
+                          ),
+                          onTap: () =>
+                              socketService.emit('vote_band', {'id': banda.id}),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: bandas.length,
-                itemBuilder: (context, index) {
-                  final Band banda = bandas[index];
-                  return Dismissible(
-                    key: Key(banda.id),
-                    direction: DismissDirection.startToEnd,
-                    background: DismissibleBackgroundBand(),
-                    onDismissed: (direction) =>
-                        socketService.emit('delete_band', {'id': banda.id}),
-                    child: BandaListTile(
-                      banda: banda,
-                      socketService: socketService,
-                    ),
-                  );
-                },
-              ),
+            floatingActionButton: FloatingActionButton(
+              elevation: 2.0,
+              onPressed: () {
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    barrierDismissible: true,
+                    pageBuilder: (context, animation, ___) {
+                      return NewBandAlertDialog(
+                        nombreBanda: nombreBanda,
+                        socketService: socketService,
+                        animation: animation,
+                      );
+                    },
+                  ),
+                );
+              },
+              child: Icon(Icons.add),
             ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          elevation: 2.0,
-          onPressed: () {
-            Navigator.of(context).push(
-              PageRouteBuilder(
-                barrierDismissible: true,
-                pageBuilder: (context, animation, ___) {
-                  return NewBandAlertDialog(
-                    nombreBanda: nombreBanda,
-                    socketService: socketService,
-                    animation: animation,
-                  );
-                },
-              ),
-            );
-          },
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
+          );
   }
 }
 
@@ -170,62 +210,18 @@ class NewBandAlertDialog extends StatelessWidget {
   }
 }
 
-class DismissibleBackgroundBand extends StatelessWidget {
-  const DismissibleBackgroundBand({
-    Key key,
-  }) : super(key: key);
+// class BandaListTile extends StatelessWidget {
+//   const BandaListTile({
+//     Key key,
+//     @required this.banda,
+//     @required this.socketService,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      color: Colors.purple,
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          'Alaos',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w700,
-            fontSize: 21.0,
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   final Band banda;
+//   final SocketService socketService;
 
-class BandaListTile extends StatelessWidget {
-  const BandaListTile({
-    Key key,
-    @required this.banda,
-    @required this.socketService,
-  }) : super(key: key);
-
-  final Band banda;
-  final SocketService socketService;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(banda.nombre),
-      subtitle: Text('Id: ${banda.id}'),
-      leading: CircleAvatar(
-        radius: 20.0,
-        backgroundColor: Colors.blue,
-        child: Text(
-          banda.nombre.substring(0, 2),
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      trailing: Text(
-        banda.votes.toString(),
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 21.0,
-        ),
-      ),
-      onTap: () => socketService.emit('vote_band', {'id': banda.id}),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+//   }
+// }
